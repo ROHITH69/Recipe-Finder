f1d80986c212facb5ddcda78b9d9902fbf741eed
rohithnemalipuri <118468756+ROHITH69@users.noreply.github.com>
2025-07-18 16:54:00 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-def format_recipe(recipe):
-    return (
-        f"🍲 Recipe Name: {recipe['name']}\n"
-        f"🕒 Time: {recipe['cook_time']} mins\n"
-        f"📋 Ingredients: {', '.join(recipe['ingredients'])}\n"
-        f"📖 Instructions: {recipe['instructions']}\n"
-    )
